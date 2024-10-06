c85147a7e232d7730187781c1b10494e913c7ac8
sirconscious <157235204+sirconscious@users.noreply.github.com>
2024-10-06 20:25:22 +0100

    HeroSection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 export default function App() {
   return (
-<div className="bg-slate-800 h-screen">
-
-<NavBar/>
+<div className="bg-slate-800  ">
+      <div className="mx-6">
+      <NavBar/>
+      </div>
+    <div className="max-w-7xl mx-auto pt-10 px-10">
+    <Hero/>
+    </div>
+    
 </div>  )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import video1 from '../assets/video1.mp4'
+import video2 from '../assets/video2.mp4'
+export default function Hero() {
+  return (
+    <div className='flex flex-col w-full mt-4 lg:mt-20 justify-center items-center'>
+     <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-white text-center md:px-20"> 
+        VirtualIR build tools <span className='bg-gradient-to-r from-cyan-400 to-cyan-700 text-transparent bg-clip-text'>
+      for developers
+      </span></h1>
+      <p className='text-white mt-10 text-center text-sm sm:text-md md:text-lg  md:px-32 lg:px-44 '>
+      Empower your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive reality!
+      </p>
+      <div className="flex justify-center items-center mt-10">
+      <a href="#" className='py-2 px-3 border border-cyan-400 rounded-md text-cyan-400
+                                hover:bg-cyan-700
+                                hover:text-white
+                                '>
+                                    Start For Free
+                                </a>
+      <a href="#" className='bg-gradient-to-r from-cyan-600 to-cyan-500 ml-5
+                    hover:from-cyan-700 hover:to-cyan-600 
+                    p-2 rounded-lg'>
+                        Documentation
+                    </a>                          
+      </div>
+      <div className="flex mt-10 justify-center p-12">
+        <video   autoPlay muted loop className='border-2 border-cyan-500 w-1/2 rounded-md ml-12 mr-2'>
+            <source src={video1} type='video/mp4'/>
+            Your browser does not suport this video tag
+        </video>
+        <video   autoPlay muted loop className='border-2 border-cyan-500 w-1/2 rounded-md mr-14 ml-2'>
+            <source src={video2} type='video/mp4'/>
+            Your browser does not suport this video tag
+        </video>
+      </div>
+    </div>
+  )
+}
